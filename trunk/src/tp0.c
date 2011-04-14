cf277567699544aa033be617b0769cd852e09ff0
martineq@gmail.com <martineq@gmail.com@42b9ae76-e309-7639-3d45-ab65b035d155>
2011-04-14 14:22:21 +0000

    Se agrega leerEntrada

--- trunk/src/tp0.c
+++ trunk/src/tp0.c
@@ -6,14 +6,75 @@
 
 typedef struct cadena{
        char* dato;
-       int V,H,S,D,err;  
+       int V,H,S,D,err;
 
 }tCadena;
 
-//prueba de uso de svn
-//char* leerEntrada();
+int main(int argc,char** argv){
 
-int contarCaracteres(char* entrada,char caracteresBuscados){ 
+    tCadena str1;
+
+    char* string1 = NULL; //para guardar la primera entrada pasada por parametro.
+    char* string2 = NULL; //para guardar la segunda entrada pasada por parametro.
+
+    char* msgerr;
+
+    //indicadores de las entradas validas
+    char* flag[] = {"-V","--version","-h","--help","-s","--squeeze","--delete","-d","-sd","-ds"};
+
+	//para ver los mensajes de ayuda/version
+	if ((argv[1][0] == '-') && (argv[1][1] == 'h')){
+		imprimirMensajeDeAyudaSegunEntrada();
+		return 0;
+	}
+	if ((argv[1][0] == '-') && (argv[1][1] == 'V')){
+		printf("Versión 0.0000001\n");
+		return 0;
+	}
+    str1.dato = leerEntrada();
+
+    guardarEntradas(argc, argv, &string1, &string2);
+
+    // Inicialar flags
+    str1.V=str1.H=str1.S=str1.D=str1.err=0;
+    definirFlagsSegunEntradas(argc, argv, flag, &str1);
+    procesarEntradas(string1, string2, &str1);
+
+    procesarErrores(str1, &msgerr);
+
+    free(str1.dato);
+    return 0;
+}
+
+char* leerEntrada(){
+	char* aux;
+	char* str1dato;
+	int fin = 0;
+	int objetosleidos = 0;
+	int tamanio = TAMANIO_BUFFER;
+
+	str1dato=malloc(sizeof(char)*tamanio);
+	aux = malloc(sizeof(char)*tamanio);
+
+	while(!fin){
+
+		objetosleidos=fread(aux,sizeof(char),TAMANIO_BUFFER,stdin);
+		aux[objetosleidos]='\0';
+
+		if (TAMANIO_BUFFER > objetosleidos)
+			fin = !fin;
+		else
+			tamanio += TAMANIO_BUFFER;
+
+		str1dato = realloc(str1dato,tamanio);
+		strcat(str1dato,aux);
+	}
+
+	free(aux);
+	return str1dato;
+}
+
+int contarCaracteres(char* entrada,char caracteresBuscados){
 	int contador = 0;
 	int w;
 	for(w=0;w<strlen(entrada);w++)
@@ -22,7 +83,7 @@ int contarCaracteres(char* entrada,char caracteresBuscados){
 	return contador;
     }
 
-char* borrarCaracteres(char* entrada,char caracterAborrar,int squeeze){ 
+char* borrarCaracteres(char* entrada,char caracterAborrar,int squeeze){
 
 	int cont = contarCaracteres(entrada,caracterAborrar);
 	char* salida = malloc(sizeof(char) * ( (strlen(entrada) - cont) + 1 )); //sumo 1 para guardar '\0'
@@ -47,7 +108,7 @@ char* reemplazarCaracteres(char* entrada,char caracterAreemplazar,char reemplazo
 	for(w=0; w < strlen(entrada); w++){
 		if (entrada[w] == caracterAreemplazar)
 			entradaReemplazada[w]=reemplazo;
-		if (entrada[w] != caracterAreemplazar)  
+		if (entrada[w] != caracterAreemplazar)
 			entradaReemplazada[w] = entrada[w];
 	}
         entradaReemplazada[strlen(entrada)]='\0';
@@ -70,10 +131,10 @@ void traducir(char** dato,char* string1,char* string2){
 
 	char* aTraducir = malloc(sizeof(char)*strlen(string1));
 	char* traductores = malloc(sizeof(char)*strlen(aux));
-	
+
 	*aTraducir= '\0';
 	*traductores = '\0';
-	
+
 	int a;
 	int b;
 	int c;
@@ -81,12 +142,12 @@ void traducir(char** dato,char* string1,char* string2){
 	int caracterRepetido;
 	int posicionChar;
 	char caracterAuxiliar;
-	
+
 	posicionNueva = 0;
 	for(a=0;a<strlen(string1);a++){
 		posicionChar = a;
 		caracterAuxiliar = *(string1+a);
-		
+
 		//verifica si ya salió anteriormente
 		caracterRepetido=0;
 		c=0;
@@ -95,7 +156,7 @@ void traducir(char** dato,char* string1,char* string2){
 				caracterRepetido=1;
 			c++;
 		}
-		
+
 		if (caracterRepetido == 0){
 		//busca la última posición del caracter leído
 		for(b=a+1;b<strlen(string1);b++){
@@ -110,10 +171,10 @@ void traducir(char** dato,char* string1,char* string2){
 		*(aTraducir+posicionNueva)='\0';
 		*(traductores+posicionNueva)='\0';
 
-			
+
 		}
 	}
-	
+
 	int index;
 	for (index = 0; index < strlen(aTraducir);index++ ){
 		*dato = reemplazarCaracteres(*dato,*(aTraducir+index),*(traductores+index));
@@ -123,6 +184,7 @@ void traducir(char** dato,char* string1,char* string2){
 	if (strlen(aux)!=strlen(string2))
 		free(aux);
 }
+
 //crea dos punteros a los string de las entradas pasadas por parametro
 void guardarEntradas(int argc, char** argv, char** string1, char** string2){
 	int p = 1;
@@ -134,14 +196,13 @@ void guardarEntradas(int argc, char** argv, char** string1, char** string2){
     	p++;
     }
  }
- 
-	 
+
 void definirFlagsSegunEntradas(int argc, char** argv, char** flag, tCadena* str1){
     int i;
-    //bandera: para salir de los ciclos, suponiendo que el formato estandar 
+    //bandera: para salir de los ciclos, suponiendo que el formato estandar
     //que se tiene que leer de linea de comando es del estilo:
-    // tp0 [options] string1 string2. mejora un poco la eficiencia 
-    int bandera = 0; 
+    // tp0 [options] string1 string2. mejora un poco la eficiencia
+    int bandera = 0;
     for (i=1;i<argc && (bandera!=1);i++){
         int j;
         for (j=0;j<10 && (bandera!=1);j++){
@@ -162,7 +223,7 @@ void definirFlagsSegunEntradas(int argc, char** argv, char** flag, tCadena* str1
 						(*str1).S=1;
 						bandera = 1;
 						break;
-						
+
                 case 6:
                 case 7:
 						(*str1).D=1;
@@ -181,7 +242,8 @@ void definirFlagsSegunEntradas(int argc, char** argv, char** flag, tCadena* str1
                 }
         }
     }
-	}
+}
+
 void imprimirMensajeDeAyudaSegunEntrada(){
 		printf("Usage:\n\ttp0 -h\n\ttp0 -V\n\ttp0 [options] string1 string2\n\ttp0 [options] string1\nOptions:\n\t-V, --version\tPrint version and quit.\n\t-h, --help\tPrint this information and quit.\n\t-d, --delete\tDelete characters in string1\n\t-s, --squeeze\tSqueeze characters in input.\n");
 	}
@@ -219,8 +281,8 @@ void procesarEntradas(char* string1, char* string2, tCadena* str1){
 		for (r = 0; r < strlen(string1);r++ )
 		    reemplazarCaracteres((*str1).dato,*(string1+r),*(string2+r));
 	}
-}	
-	
+}
+
 void procesarErrores(tCadena str1, char** msgerr){
 	  if (str1.err!=0) {  //Chequea si existen errores
         switch ( str1.err ) {
@@ -242,42 +304,4 @@ void procesarErrores(tCadena str1, char** msgerr){
 	fwrite(*msgerr, sizeof(char),strlen(*msgerr),stderr);//Si existió algún error, sale el mensaje por stderr
     }else
     	fwrite(str1.dato, sizeof(char),strlen(str1.dato),stdout);
-}
-
-int main(int argc,char** argv){
-
-    tCadena str1;
-
-    char* string1 = NULL; //para guardar la primera entrada pasada por parametro.
-    char* string2 = NULL; //para guardar la segunda entrada pasada por parametro.
-
-    char* msgerr; 
-
-    //indicadores de las entradas validas
-    char* flag[] = {"-V","--version","-h","--help","-s","--squeeze","--delete","-d","-sd","-ds"};
-	
-	//para ver los mensajes de ayuda/version
-	if ((argv[1][0] == '-') && (argv[1][1] == 'h')){
-		imprimirMensajeDeAyudaSegunEntrada();
-		return 0;
-	} 
-	if ((argv[1][0] == '-') && (argv[1][1] == 'V')){
-		printf("Versión 0.0000001\n");
-		return 0;
-	} 
-
-			//
-			//str1.dato = leerEntrada();
-
-    guardarEntradas(argc, argv, &string1, &string2);
-
-    // Inicialar flags
-    str1.V=str1.H=str1.S=str1.D=str1.err=0;
-    definirFlagsSegunEntradas(argc, argv, flag, &str1);
-    procesarEntradas(string1, string2, &str1);
-    
-    procesarErrores(str1, &msgerr);
-    
-    free(str1.dato);
-    return 0;
 }
